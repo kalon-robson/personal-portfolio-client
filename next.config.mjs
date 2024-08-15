a757295c805029b0b6613e0785a7afacf5ff19d2
Kalon Robson <kalon.robson@innovixx.co.uk>
2024-08-15 14:10:08 +0000

    feat: add environment variables for server and client URLs

--- next.config.mjs
+++ next.config.mjs
@@ -5,18 +5,34 @@ const nextConfig = {
     locales: ['en', 'es'],
   },
   images: {
-    domains: [
-      new URL(process.env.NEXT_PUBLIC_SERVER_URL).hostname,
-      'via.placeholder.com',
+    remotePatterns: [
+      {
+        hostname: new URL(process.env.NEXT_PUBLIC_SERVER_URL).hostname,
+        port: new URL(process.env.NEXT_PUBLIC_SERVER_URL).port,
+        protocol: process.env.NODE_ENV === 'production' ? 'http' : 'https',
+      },
     ],
   },
+  pageExtensions: ['tsx', 'js'],
   publicRuntimeConfig: {
-    SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL,
+    SERVER_URL: process.env.NEXT_PUBLIC_CLIENT_URL,
   },
   rewrites: async () => [
     {
       destination: `${process.env.NEXT_PUBLIC_SERVER_URL}/media/:path*`,
       source: '/media/:path*',
+    },
+    {
+      destination: `${process.env.NEXT_PUBLIC_SERVER_URL}/video/:path*`,
+      source: '/video/:path*',
+    },
+    {
+      destination: `${process.env.NEXT_PUBLIC_SERVER_URL}/admin/:path*`,
+      source: '/admin/:path*',
+    },
+    {
+      destination: `${process.env.NEXT_PUBLIC_SERVER_URL}/api/:path*`,
+      source: '/api/:path*',
     },
   ],
 };
